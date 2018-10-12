58b1911a43e5b5a84c1348daa8126c35a6d3c7bc
Jacob Zimmermann <Jacob.Zimmermann@cerner.com>
2018-10-12 17:01:18 -0500

    Forgot to build the request first

--- mtg-fetcher.js
+++ mtg-fetcher.js
@@ -2,29 +2,29 @@ const baseURI = 'https://api.magicthegathering.io/v1';
 
 function fetch(cmc, color, power_toughness, name)
 {
-    var request = [];
-    request.push(baseURI);
+    var requestParts = [];
+    requestParts.push(baseURI);
     if(cmc != null)
     {
-        request.push("&cmc=", cmc);
+        requestParts.push("&cmc=", cmc);
     }
     if(color != null)
     {
-        request.push("&colors=", color);
+        requestParts.push("&colors=", color);
     }
     if(power_toughness != null)
     {
         power_toughness = power_toughness.replace("/", "\\");
         var power = power_toughness.split("\\")[0];
         var toughness = power_toughness.split("\\")[1];
-        request.push("&power=", power);
-        request.push("&toughness=", toughness);
+        requestParts.push("&power=", power);
+        requestParts.push("&toughness=", toughness);
     }
     if(name != null)
     {
-        request.push("&name:", name);
+        requestParts.push("&name:", name);
     }
-    request = request.replace("&", "");
+    var request = requestParts.join("").replace("&", "");
     getResponse(request);
     //return cards[Math.floor(Math.random() * cards.size())];
 }
